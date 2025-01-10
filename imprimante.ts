c6da654e469e5915d4d15b5b7e8c19a7b271c0ab
Jey-T <jeantheo.sambafouala@gmail.com>
2025-01-10 17:52:04 +0100

    Initial commit.

---
+++ imprimante.ts
@@ -0,0 +1,34 @@
+//En temps O(n + m)
+//En espace O(1)
+function checkIfprintable(availableLetters: string, input: string) {
+
+    if (availableLetters.length < input.length) {
+        return false
+    }
+
+    //Tableau représentant un compteur pour chaque character ASCII,  O(1) en espace
+    const alphabet: number[] = new Array(128, 0)
+
+    //Première boucle pour déterminer les lettres disponibles, O(n) avec n la longeur d'availableLetters.
+    for (let i = 0; i < availableLetters.length; i++) {
+        // O(1) en temps pour l'accès.
+        alphabet[availableLetters.charCodeAt(i)]++ 
+    }
+
+    //Seconde boucle pour determiner si on a asser de lettres pour imprimer la string, O(m) avec m la longeur d'input.
+    for (let j = 0; j < input.length; j++) {
+        // O(1) en temps pour l'accès.
+        if (--alphabet[input.charCodeAt(j)] < 0) {
+            return false
+        }
+    }
+
+    return true
+}
+
+
+console.log(checkIfprintable("aabbcc", "abc")); // true
+console.log(checkIfprintable("aabbcc", "aabbccc")); // false
+console.log(checkIfprintable("xyz", "zxy")); // true
+console.log(checkIfprintable("", "a")); // false
+console.log(checkIfprintable("aaa", "")); // true
